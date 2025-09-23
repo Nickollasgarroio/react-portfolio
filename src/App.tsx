47e1d878c3a0e303aee63c7a780ea9b920fb74e6
Nickollas G. <111729784+Nickollasgarroio@users.noreply.github.com>
2025-09-23 18:10:57 -0300

    fixing issues with gh pages deploy

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,16 @@ import CurriculumPage from "@/pages/curriculum";
 import AboutPage from "@/pages/about";
 import CartaAoRecutradorPage from "@/pages/cartaAoRecrutador";
 import ContactPage from "@/pages/contact";
-import RedirectToHash from "./404";
 
 function App() {
   return (
-    <HashRouter>
-      <RedirectToHash />
-      <Routes>
-        <Route element={<IndexPage />} path="/" />
-        <Route element={<AboutPage />} path="/about" />
-        <Route element={<CurriculumPage />} path="/curriculum" />
-        <Route element={<CartaAoRecutradorPage />} path="/recruiter" />
-        <Route element={<ContactPage />} path="/contact" />
-      </Routes>
-    </HashRouter>
+    <Routes>
+      <Route element={<IndexPage />} path="/" />
+      <Route element={<AboutPage />} path="/about" />
+      <Route element={<CurriculumPage />} path="/curriculum" />
+      <Route element={<CartaAoRecutradorPage />} path="/recruiter" />
+      <Route element={<ContactPage />} path="/contact" />
+    </Routes>
   );
 }
 
---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+import { useEffect } from "react";
+
+export default function NotFoundPage() {
+  <p>erro</p>;
+
+  return null; // esse componente não renderiza nada
+}
