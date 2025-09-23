96603e152df6d61532bd248d3be03496615ea2a1
Nickollas G. <111729784+Nickollasgarroio@users.noreply.github.com>
2025-09-23 18:36:37 -0300

    update routing paths in App component and set basename for BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import "@/styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider>
         <App />
       </Provider>
